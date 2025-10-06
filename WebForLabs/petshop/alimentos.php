1d63e14c29d728b439880a2e95d16e723bd607f2
LUIS C. ZAMORA G. <luis.zamora012@gmail.com>
2025-10-05 23:40:43 -0600

    Aplique el footer ya con un archivo shared
    Elimine las lineas de cada archivo php que tenia footer escrito manualmente y use ya el que esta en la carpeta shared, para usar un footer en comun en todas. Me falta solamente editarle el contenido.

--- WebForLabs/petshop/alimentos.php
+++ WebForLabs/petshop/alimentos.php
@@ -94,9 +94,5 @@
     </main>
 
 
-    <footer></footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <!-- Seccion de mi main -->
+    <?php include "shared/footer.php"; ?>
--- WebForLabs/petshop/index.php
+++ WebForLabs/petshop/index.php
@@ -64,9 +64,4 @@
     </main>
 
     <!-- Pie de la página -->
-    <footer></footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <?php include "shared/footer.php"; ?>
